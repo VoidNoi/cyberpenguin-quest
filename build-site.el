cd9442aebd6adc8ef578b62183f8c30d8e45608d
VoidNoi <voidnoi@proton.me>
2025-05-22 21:52:17 +0200

    Add css

--- build-site.el
+++ build-site.el
@@ -17,23 +17,32 @@
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
-      org-html-htmlize-output-type nil        ;;disables highlighting by htmlize
-      org-html-head "<link rel=\"stylesheet\" href=\"style.css\" />"
+      org-html-htmlize-output-type 'css        ;;disables highlighting by htmlize
+      org-html-head "<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\" />"
       )
 
 ;; Define the publishing project
 (setq org-publish-project-alist
-      (list
-       (list "cyberpenguin-quest:main"
+      '(("cyberpenguin-quest:main"
              :recursive t
+             :base-extension "org"
              :base-directory "./content"
-             :publishing-function 'org-html-publish-to-html
+             :publishing-function org-html-publish-to-html
              :publishing-directory "./public"
              :with-author nil
-             :with-creator t
-             :with-toc t
+             :with-creator nil
+             :with-title nil
+             :with-toc nil
              :section-numbers nil
-             :time-stamp-file nil)))
+             :time-stamp-file nil
+             )
+        ("cyberpenguin-quest:assets"
+         :base-directory "./assets"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|woff2\\|ttf\\|zip"
+         :publishing-directory "./public"
+         :recursive t
+         :publishing-function org-publish-attachment)
+        ))
 
 ;; Generate the site output
 (org-publish-all t)
